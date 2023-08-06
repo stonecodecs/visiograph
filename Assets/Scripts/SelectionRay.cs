5c0e5a2cb67a449ed1e0ce9b6fe19b5e1440cf69
stonecodecs <syang477@wisc.edu>
2023-08-05 21:04:06 -0700

    added ray script and GUI that appears whenever ray hits a collider

--- Assets/Scripts/SelectionRay.cs
+++ Assets/Scripts/SelectionRay.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 public class SelectionRay : MonoBehaviour
 {
     public float raycastDistance = Mathf.Infinity; // Maximum distance for the raycast
+    public GameObject guiCircle;
 
     private Camera mainCamera;
 
     private void Start()
     {
         mainCamera = Camera.main;
+        guiCircle = GameObject.Find("GUICircle");
+        guiCircle.SetActive(false);
+
+        if (guiCircle == null)
+        {
+            Debug.LogError("GUI circle not found in the scene. Make sure the GUICircle GameObject has the GUICircle script attached.");
+            enabled = false;
+        }
+
         if (mainCamera == null)
         {
             Debug.LogError("Main camera not found in the scene. Please add a camera tagged as 'MainCamera'.");
@@ -30,12 +40,15 @@ public class SelectionRay : MonoBehaviour
         {
             Debug.DrawRay(ray.origin, ray.direction * raycastDistance, Color.red);
             // TODO: On hit, give option to select it and display information
-            Debug.Log("Raycast hit: " + hit.collider.gameObject.name);
+            Debug.Log("Raycast hit: " + hit.collider.gameObject.tag);
+            guiCircle.SetActive(true);
+            guiCircle.transform.position = hit.point;
         }
         else
         {
             // for visualization, possibly delete later
             Debug.DrawRay(ray.origin, ray.direction * raycastDistance, Color.white);
+            guiCircle.SetActive(false);
         }
     }
 }
