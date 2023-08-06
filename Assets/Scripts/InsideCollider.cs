9720b615a560935ffb678c5221f117fa7a34a445
stonecodecs <syang477@wisc.edu>
2023-08-06 14:14:49 -0700

    deactivate solid colliders when inside a model, done

--- Assets/Scripts/InsideCollider.cs
+++ Assets/Scripts/InsideCollider.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class InsideCollider : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject internalCollider; // Reference to the smaller internal solid collider
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        Debug.Log(other.tag + " entered");
+        // Check if the entering collider is the Player collider
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log(other.tag + " entered, disable");
+            // Deactivate the internal collider
+            internalCollider.SetActive(false);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit(Collider other)
     {
-        // TODO: When player hits outside non-solid collider, deactivate the internal solid collider
+        Debug.Log(other.tag + " exited");
+        // Check if the exiting collider is the Player collider
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log(other.tag + " enable");
+            // Activate the internal collider
+            internalCollider.SetActive(true);
+        }
     }
 }
